bd6c9bf691a44286f1b4d4aa4a815e3d42880fc8
cristina1991 <cristina.pereteatco@gmail.com>
2022-03-12 13:26:09 +0200

    Initial Commit (#1)

---
+++ Library.API/Constants/RouteConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.API.Constants
+{
+    public class RouteConstants
+    {
+        public const string RouteBase = "/api";
+        public const string RouteHealth = "health";
+    }
+}
---
+++ Library.API/Controllers/HealthController.cs
@@ -0,0 +1,36 @@
+﻿using Library.API.Constants;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Library.API.Controllers
+{
+    [Route(RouteConstants.RouteHealth)]
+    public class HealthController : Controller
+    {
+
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(); 
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+    }
+}

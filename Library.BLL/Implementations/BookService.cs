3607a1012a182a75115eab19549b7a5c02c4cabd
theDotAfterExe <valentin.filip@student.uaic.ro>
2022-03-27 21:01:51 +0300

    Edited BookController and BookService

--- Library.BLL/Implementations/BookService.cs
+++ Library.BLL/Implementations/BookService.cs
@@ -1,26 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Library.BLL.Dto;
 using Library.BLL.Interfaces;
+using Library.Data.Entities;
+using Library.Data.Interfaces;
 
 namespace Library.BLL.Implementations
 {
     public class BookService : IBookService
     {
-        public BookService()
+        private readonly IRepository<Book> _repository;
+        private readonly IMapper _mapper;
+        public BookService(IRepository<Book> repository, IMapper mapper)
         {
-            
+            _repository = repository;
+            _mapper = mapper;
         }
         public async Task<IList<BookDto>> GetAll()
         {
-            throw new NotImplementedException();
+            var entities = await _repository.GetAllAsync();
+            return _mapper.Map<IList<BookDto>>(entities);
         }
 
         public async Task<BookDto> Get(int id)
         {
-            throw new NotImplementedException();
+            var entities = await _repository.GetAllAsync();
+            var entityById = entities.SingleOrDefault(e => e.Id == id);
+            var mapped = _mapper.Map<BookDto>(entityById);
+
+            return mapped;
         }
 
         public async Task<BookDto> Add(BookDto model)
